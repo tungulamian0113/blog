174e338c9f64c3c4790adf41626f4c3119ad0bb0
tungulamian0113 <lyfportfolio@outlook.com>
2026-03-06 11:56:59 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,7 @@ import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   adapter: netlify(), // Set adapter for deployment, or set `linkCard` to `false` in `src/config.ts`
-  site: lyfportfolio.dpdns.org,
+  site: https://lyfportfolio.dpdns.org/,
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
